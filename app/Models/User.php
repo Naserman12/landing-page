1288281ad45d211745bf859c763567dcdd851b42
Naser Man <tgza54@gmail.com>
2025-08-10 19:01:57 +0300

    link database

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
 
+
+    use HasApiTokens, HasFactory, Notifiable ,HasRoles;
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -22,6 +26,7 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+   
 
     /**
      * The attributes that should be hidden for serialization.
